637e6c714f5a55c71066d03b00b9d325a1f51fc1
Torcetta Alessandro Orazio Fausto <a.torcetta@reply.it>
2021-11-11 16:20:27 +0100

    refs #heroku - url per deploy su heroku

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 require('dotenv').config();
 
+const baseURL = process.env.HEROKU_URL + "api";
+
 export default axios.create({
-  baseURL: process.env.HEROKU_URL + "api",
+  baseURL: baseURL,
   headers: {
     "Content-type": "application/json"
   }
