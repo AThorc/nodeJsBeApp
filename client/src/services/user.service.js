0dc8663a41cc422b858bf64da3b3d45ec5135a6e
Torcetta Alessandro Orazio Fausto <a.torcetta@reply.it>
2021-11-11 15:39:06 +0100

    refs #heroku - url per deploy su heroku

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/api/test/';
+const API_URL = process.env.HEROKU_URL + "pi/test/";
 
 class UserService {
   getPublicContent() {
