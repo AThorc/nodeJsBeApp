4686f3268ae0d00b07c06cc9623b074c44ad1ad1
Torcetta Alessandro Orazio Fausto <a.torcetta@reply.it>
2021-12-26 17:11:36 +0100

    refs #mern - aggiunto tab partner

---
+++ client/src/components/AddSegnalatore.js
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import SegnalatoreDataService from "../services/SegnalatoreService";
+
+import AuthService from "../services/auth.service";
+
+const AddSegnalatore = () => {
+  const initialSegnalatoreState = {
+    id: null,
+    denominazione: "",
+    dataInizio: "",
+    fatturatoSegnalatore: ""
+
+  };
+  const [segnalatore, setSegnalatore] = useState(initialSegnalatoreState);
+  const [submitted, setSubmitted] = useState(false);
+
+  const user = AuthService.getCurrentUser();
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setSegnalatore({ ...segnalatore, [name]: value });
+  };
+
+  const saveSegnalatore = () => {
+    if(user){
+      var data = {        
+        denominazione: segnalatore.denominazione,
+        dataInizio: segnalatore.dataInizio,
+        fatturatoSegnalatore: segnalatore.fatturatoSegnalatore
+      };
+
+      SegnalatoreDataService.create(data)
+      .then(response => {
+        setSegnalatore({
+          id: response.data.id,
+          denominazione: response.data.denominazione,
+          dataInizio: response.data.dataInizio,
+          fatturatoSegnalatore: response.data.fatturatoSegnalatore
+        });
+        setSubmitted(true);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    }   
+  };
+
+  const newSegnalatore = () => {
+    setSegnalatore(initialSegnalatoreState);
+    setSubmitted(false);
+  };
+
+
+  if(user){
+    return (
+      <div className="submit-form">
+        {submitted ? (
+          <div>
+            <h4>You submitted successfully!</h4>
+            <button className="btn btn-success" onClick={newSegnalatore}>
+              Add
+            </button>
+          </div>
+        ) : (
+          <div>
+            <div className="form-group">
+              <label htmlFor="title">Denominazione</label>
+              <input
+                type="text"
+                className="form-control"
+                id="denominazione"
+                required
+                value={segnalatore.denominazione}
+                onChange={handleInputChange}
+                name="denominazione"
+              />
+            </div>
+  
+            <div className="form-group">
+              <label htmlFor="title">Data inizio</label>
+              <input
+                type="date"
+                className="form-control"
+                id="dataInizio"
+                required
+                value={segnalatore.dataInizio}
+                onChange={handleInputChange}
+                name="dataInizio"
+              />
+            </div>
+  
+            <div className="form-group">
+              <label htmlFor="title">Fatturato segnalatore</label>
+              <input
+                type="number"
+                className="form-control"
+                id="fatturatoSegnalatore"
+                required
+                value={segnalatore.fatturatoSegnalatore}
+                onChange={handleInputChange}
+                name="fatturatoSegnalatore"
+              />
+            </div>           
+  
+            <button onClick={saveSegnalatore} className="btn btn-success">
+              Conferma
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }else{
+    return(
+      <div>
+        <br />
+          <p>Effettua il login per vedere i segnalatori...</p>
+      </div>
+    );
+  }
+  
+};
+
+export default AddSegnalatore;
---
+++ client/src/components/Partner.js
@@ -0,0 +1,159 @@
+import React, { useState, useEffect } from "react";
+import PartnerDataService from "../services/PartnerService";
+
+import AuthService from "../services/auth.service";
+
+import ConfirmDialog from "./confirmDialog.component";
+
+import moment from 'moment'
+
+const Partner = props => {
+  const initialPartnerState = {
+    id: null,
+    denominazione: "",
+    dataInizio: "",
+    fatturatoPartner: ""    
+  };
+  const [currentPartner, setCurrentPartner] = useState(initialPartnerState);
+  const [message, setMessage] = useState("");
+
+  const user = AuthService.getCurrentUser();
+
+  const getPartner = id => {
+    if(user){
+      PartnerDataService.get(id)
+      .then(response => {
+        setCurrentPartner(response.data);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    }
+    
+  };
+
+  useEffect(() => {
+    getPartner(props.match.params.id);
+  }, [props.match.params.id]);
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setCurrentPartner({ ...currentPartner, [name]: value });
+  };  
+
+  const updatePartner = () => {
+    if(user){
+      PartnerDataService.update(currentPartner.id, currentPartner)
+      .then(response => {
+        console.log(response.data);
+        setMessage("The partner was updated successfully!");
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    }
+   
+  };
+
+  const deletePartner = () => {
+    if(user){
+      PartnerDataService.remove(currentPartner.id)
+      .then(response => {
+        console.log(response.data);
+        props.history.push("/partners");
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    }
+    
+  };
+
+  if(user){
+    return (
+      <div>
+        {currentPartner ? (
+          <div className="edit-form">
+            <h4>Partner</h4>
+            <form>
+              <div className="form-group">
+                <label htmlFor="title">Denominazione</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="denominazione"
+                  required
+                  value={currentPartner.denominazione}
+                  onChange={handleInputChange}
+                  name="denominazione"
+                />
+              </div>
+  
+              <div className="form-group">
+                <label htmlFor="title">Data inizio</label>
+                <input
+                  type="date"
+                  className="form-control"
+                  id="dataInizio"
+                  required
+                  value={moment(currentPartner.dataInizio).format('YYYY-MM-DD')}        
+                  onChange={handleInputChange}
+                  name="dataInizio"
+                />
+              </div>
+  
+              <div className="form-group">
+                <label htmlFor="title">Fatturato partner</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="fatturatoPartner"
+                  required
+                  value={currentPartner.fatturatoPartner}
+                  onChange={handleInputChange}
+                  name="fatturatoPartner"
+                />
+              </div>               
+            </form>          
+  
+            <ConfirmDialog 
+              title= 'Delete'
+              message= 'Sei sicuro di voler cancellare il partner?'
+              onClickYes= {deletePartner}
+              className="btn btn-danger"
+            />
+
+            <button className="badge badge-danger mr-2 d-none" onClick={deletePartner}>
+              Delete
+            </button>
+  
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={updatePartner}
+            >
+              Update
+            </button>
+            <p>{message}</p>
+          </div>
+        ) : (
+          <div>
+            <br />
+            <p>Please click on a Partner...</p>
+          </div>
+        )}
+      </div>
+    );
+  }else{
+    return(
+      <div>
+        <br />
+          <p>Effettua il login per vedere i partners...</p>
+      </div>
+    );
+  }
+  
+};
+
+export default Partner;
