60277109511523a9346018d56987b908e1ab8240
Torcetta Alessandro Orazio Fausto <a.torcetta@reply.it>
2021-12-30 13:17:45 +0100

    refs #mern - modificato stile e aggiunti logo e svg
    (cherry picked from commit 0b3bc5782af5c13e149060417028dd198f55b0cd)

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+import svg from "./images/multifinance-svg.svg"
+
 /*
 import AddTutorial from "./components/AddTutorial";
 import Tutorial from "./components/Tutorial";
@@ -74,9 +76,9 @@ class App extends Component {
 
     return (
       <div>
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-md navbar-dark orange">
           <Link to={"/"} className="navbar-brand">
-            Multifinance
+          <img src={svg} class="img-fluid" alt="Metodo Multifinance"/>
           </Link>
 
          
@@ -193,39 +195,4 @@ class App extends Component {
   }
 }
 
-/*
-function App() {
-  return (
-    
-    <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/tutorials" className="navbar-brand">
-          ThorcApp
-        </a>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/tutorials"} className="nav-link">
-              Tutorials
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/add"} className="nav-link">
-              Add
-            </Link>
-          </li>
-        </div>
-      </nav>
-
-      <div className="container mt-3">
-        <Switch>
-          <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
-          <Route exact path="/add" component={AddTutorial} />
-          <Route path="/tutorials/:id" component={Tutorial} />
-        </Switch>
-      </div>
-    </div>
-    
-  );
-}
-*/
 export default App;
