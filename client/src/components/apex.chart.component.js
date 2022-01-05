57284f0f42285bdf07fd90cefaf21166022bbb0b
Torcetta Alessandro Orazio Fausto <a.torcetta@reply.it>
2022-01-05 18:03:58 +0100

    refs #mern - inizio implementazione bar chart custom colori

--- client/src/components/apex.chart.component.js
+++ client/src/components/apex.chart.component.js
@@ -5,6 +5,13 @@ class ApexChart extends Component {
   constructor(props) {
     super(props);
 
+    function generateRandomColor()
+    {
+        var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        return randomColor;
+        //random color will be freshly served
+    }
+
     this.state = {
         options: {
             chart: {
@@ -29,11 +36,32 @@ class ApexChart extends Component {
             },
             ],
             colors: [  function ({ value, seriesIndex, dataPointIndex, w }) {
-                if (dataPointIndex == 1) {
-                  return "#7E36AF";
-                } else {
-                  return "#D9534F";
-                }
+                if (dataPointIndex == 0) {
+                  return "#F3B415";
+                }else if (dataPointIndex == 1){
+                    return "#F27036";
+                }else if (dataPointIndex == 2){
+                    return "#663F59";
+                }else if (dataPointIndex == 3){
+                    return "#6A6E94";
+                }else if (dataPointIndex == 4){
+                    return "#4E88B4";
+                }else if (dataPointIndex == 5){
+                    return "#00A7C6";
+                }else if (dataPointIndex == 6){
+                    return "#18D8D8";
+                }else if (dataPointIndex == 7){
+                    return "#A9D794";
+                }else if (dataPointIndex == 8){
+                    return "#46AF78";
+                }else if (dataPointIndex == 9){
+                    return "#A93F55";
+                }else if (dataPointIndex == 10){
+                    return "#8C5E58";
+                }else {
+                    return generateRandomColor();
+                } 
+                
               }
         ],
         }
